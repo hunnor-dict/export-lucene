44252df092463565d28d715ec5b6107e2fff8749
Ádám Z. Kövér <adamzkover@gmail.com>
2016-11-18 18:11:35 +0100

    Move conversion methods from the model object to the indexer

--- src/main/java/net/hunnor/dict/lucene/IndexHandler.java
+++ src/main/java/net/hunnor/dict/lucene/IndexHandler.java
@@ -2,7 +2,9 @@ package net.hunnor.dict.lucene;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.lucene.analysis.Analyzer;
@@ -12,6 +14,7 @@ import org.apache.lucene.analysis.PerFieldAnalyzerWrapper;
 import org.apache.lucene.analysis.hu.HungarianAnalyzer;
 import org.apache.lucene.analysis.no.NorwegianAnalyzer;
 import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
 import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
@@ -37,6 +40,128 @@ public class IndexHandler {
 
 	private String indexDir;
 	private String spellingDir;
+
+	/**
+	 * Convert a Lucene document to a model object.
+	 * @param document the Lucene document to process
+	 * @return a new model object with data from the Lucene document
+	 */
+	public Entry getEntryFromDocument(final Document document) {
+
+		Entry entry = new Entry();
+
+		entry.setLang(document.get(IndexFields.LANG));
+		entry.setId(document.get(IndexFields.ID));
+
+		String[] roots = document.getValues(IndexFields.ROOTS);
+		if (roots.length > 0) {
+			List<String> rootList = new ArrayList<>();
+			for (String root: roots) {
+				rootList.add(root);
+			}
+			entry.setRoots(rootList);
+		}
+
+		String[] forms = document.getValues(IndexFields.FORMS);
+		if (forms.length > 0) {
+			List<String> formList = new ArrayList<>();
+			for (String form: forms) {
+				formList.add(form);
+			}
+			entry.setForms(formList);
+		}
+
+		String[] trans = document.getValues(IndexFields.TRANS);
+		if (trans.length > 0) {
+			List<String> transList = new ArrayList<>();
+			for (String tr: trans) {
+				transList.add(tr);
+			}
+			entry.setTrans(transList);
+		}
+
+		String[] quote = document.getValues(IndexFields.QUOTE);
+		if (quote.length > 0) {
+			List<String> quoteList = new ArrayList<>();
+			for (String q: quote) {
+				quoteList.add(q);
+			}
+			entry.setQuote(quoteList);
+		}
+
+		String[] quoteTrans = document.getValues(IndexFields.QUOTETRANS);
+		if (quoteTrans.length > 0) {
+			List<String> quoteTransList = new ArrayList<>();
+			for (String qTr: quoteTrans) {
+				quoteTransList.add(qTr);
+			}
+			entry.setQuoteTrans(quoteTransList);
+		}
+
+		entry.setText(document.get(IndexFields.TEXT));
+
+		return entry;
+
+	}
+
+	/**
+	 * Convert a model object to a Lucene document.
+	 * @param entry the domain object to process
+	 * @return a new Lucene document with data from the model object
+	 */
+	public Document toLuceneDocument(final Entry entry) {
+		String rootsField = IndexFields.HU_ROOTS;
+		String formsField = IndexFields.HU_FORMS;
+		String transField = IndexFields.HU_TRANS;
+		String quoteField = IndexFields.HU_QUOTE;
+		String quoteTransField = IndexFields.HU_QUOTETRANS;
+		if ("no".equals(entry.getLang())) {
+			rootsField = IndexFields.NO_ROOTS;
+			formsField = IndexFields.NO_FORMS;
+			transField = IndexFields.NO_TRANS;
+			quoteField = IndexFields.NO_QUOTE;
+			quoteTransField = IndexFields.NO_QUOTETRANS;
+		}
+
+		Document document = new Document();
+		if (entry.getLang() != null) {
+			document.add(new Field(IndexFields.LANG, entry.getLang(), Field.Store.YES, Field.Index.ANALYZED));
+		}
+		if (entry.getId() != null) {
+			document.add(new Field(IndexFields.ID, entry.getId(), Field.Store.YES, Field.Index.ANALYZED));
+		}
+		if (entry.getRoots() != null) {
+			for (String root: entry.getRoots()) {
+				document.add(new Field(rootsField, root, Field.Store.YES, Field.Index.ANALYZED));
+			}
+		}
+		if (entry.getForms() != null) {
+			for (String form: entry.getForms()) {
+				document.add(new Field(formsField, form, Field.Store.NO, Field.Index.ANALYZED));
+			}
+		}
+		if (entry.getTrans() != null) {
+			for (String tr: entry.getTrans()) {
+				document.add(new Field(transField, tr, Field.Store.NO, Field.Index.ANALYZED));
+			}
+		}
+		if (entry.getQuote() != null) {
+			for (String q: entry.getQuote()) {
+				document.add(new Field(quoteField, q, Field.Store.NO, Field.Index.ANALYZED));
+			}
+		}
+		if (entry.getQuoteTrans() != null) {
+			for (String qTr: entry.getQuoteTrans()) {
+				document.add(new Field(quoteTransField, qTr, Field.Store.NO, Field.Index.ANALYZED));
+			}
+		}
+
+		if (entry.getText() != null) {
+			document.add(new Field(IndexFields.TEXT, entry.getText(), Field.Store.YES, Field.Index.NOT_ANALYZED));
+		}
+
+		return document;
+	}
 
 	public void closeIndexReader() throws IOException {
 		indexReader.close();
@@ -72,11 +197,11 @@ public class IndexHandler {
 
 	public Entry read(int id) throws IOException {
 		Document document = indexReader.document(id);
-		return new Entry(document);
+		return getEntryFromDocument(document);
 	}
 
 	public void write(Entry indexObject) throws IOException {
-		Document luceneDocument = indexObject.toLuceneDocument();
+		Document luceneDocument = toLuceneDocument(indexObject);
 		indexWriter.addDocument(luceneDocument);
 	}
 
--- src/main/java/net/hunnor/dict/lucene/model/Entry.java
+++ src/main/java/net/hunnor/dict/lucene/model/Entry.java
@@ -1,199 +1,184 @@
 package net.hunnor.dict.lucene.model;
 
-import java.util.ArrayList;
 import java.util.List;
-
-import org.apache.lucene.document.Document;
-import org.apache.lucene.document.Field;
-
-import net.hunnor.dict.lucene.IndexFields;
 
 /**
  * Model object for a dictionary entry.
  */
 public class Entry {
 
+	/**
+	 * The language of the entry.
+	 */
 	private String lang;
+
+	/**
+	 * The ID of the entry.
+	 */
 	private String id;
+
+	/**
+	 * Root forms of the entry.
+	 */
 	private List<String> roots;
+
+	/**
+	 * Inflected forms of the entry.
+	 */
 	private List<String> forms;
+
+	/**
+	 * Translations of the entry.
+	 */
 	private List<String> trans;
+
+	/**
+	 * Usage examples for the entry.
+	 */
 	private List<String> quote;
+
+	/**
+	 * Translations of usage examples.
+	 */
 	private List<String> quoteTrans;
 
+	/**
+	 * The HTML content of the entry.
+	 */
 	private String text;
 
+	/**
+	 * Hide default constructor.
+	 */
 	public Entry() {
 	}
 
-	public Entry(Document document) {
-		this.lang = document.get(IndexFields.LANG);
-		this.id = document.get(IndexFields.ID);
-
-		String[] roots = document.getValues(IndexFields.ROOTS);
-		if (roots.length > 0) {
-			List<String> rootList = new ArrayList<>();
-			for (String root: roots) {
-				rootList.add(root);
-			}
-			this.setRoots(rootList);
-		}
-
-		String[] forms = document.getValues(IndexFields.FORMS);
-		if (forms.length > 0) {
-			List<String> formList = new ArrayList<>();
-			for (String form: forms) {
-				formList.add(form);
-			}
-			this.setForms(formList);
-		}
-
-		String[] trans = document.getValues(IndexFields.TRANS);
-		if (trans.length > 0) {
-			List<String> transList = new ArrayList<>();
-			for (String tr: trans) {
-				transList.add(tr);
-			}
-			this.setTrans(transList);
-		}
-
-		String[] quote = document.getValues(IndexFields.QUOTE);
-		if (quote.length > 0) {
-			List<String> quoteList = new ArrayList<>();
-			for (String q: quote) {
-				quoteList.add(q);
-			}
-			this.setQuote(quoteList);
-		}
-
-		String[] quoteTrans = document.getValues(IndexFields.QUOTETRANS);
-		if (quoteTrans.length > 0) {
-			List<String> quoteTransList = new ArrayList<>();
-			for (String qTr: quoteTrans) {
-				quoteTransList.add(qTr);
-			}
-			this.setQuoteTrans(quoteTransList);
-		}
-
-		this.text = document.get(IndexFields.TEXT);
-	}
-
-	public Document toLuceneDocument() {
-		String rootsField = IndexFields.HU_ROOTS;
-		String formsField = IndexFields.HU_FORMS;
-		String transField = IndexFields.HU_TRANS;
-		String quoteField = IndexFields.HU_QUOTE;
-		String quoteTransField = IndexFields.HU_QUOTETRANS;
-		if ("no".equals(lang)) {
-			rootsField = IndexFields.NO_ROOTS;
-			formsField = IndexFields.NO_FORMS;
-			transField = IndexFields.NO_TRANS;
-			quoteField = IndexFields.NO_QUOTE;
-			quoteTransField = IndexFields.NO_QUOTETRANS;
-		}
-
-		Document document = new Document();
-		if (lang != null) {
-			document.add(new Field(IndexFields.LANG, lang, Field.Store.YES, Field.Index.ANALYZED));
-		}
-		if (id != null) {
-			document.add(new Field(IndexFields.ID, id, Field.Store.YES, Field.Index.ANALYZED));
-		}
-		if (roots != null) {
-			for (String root: roots) {
-				document.add(new Field(rootsField, root, Field.Store.YES, Field.Index.ANALYZED));
-			}
-		}
-		if (forms != null) {
-			for (String form: forms) {
-				document.add(new Field(formsField, form, Field.Store.NO, Field.Index.ANALYZED));
-			}
-		}
-		if (trans != null) {
-			for (String tr: trans) {
-				document.add(new Field(transField, tr, Field.Store.NO, Field.Index.ANALYZED));
-			}
-		}
-		if (quote != null) {
-			for (String q: quote) {
-				document.add(new Field(quoteField, q, Field.Store.NO, Field.Index.ANALYZED));
-			}
-		}
-		if (quoteTrans != null) {
-			for (String qTr: quoteTrans) {
-				document.add(new Field(quoteTransField, qTr, Field.Store.NO, Field.Index.ANALYZED));
-			}
-		}
-
-		if (text != null) {
-			document.add(new Field(IndexFields.TEXT, text, Field.Store.YES, Field.Index.NOT_ANALYZED));
-		}
-
-		return document;
-	}
-
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
+	/**
+	 * Return the language of the entry.
+	 * @return the language of the entry
+	 */
 	public String getLang() {
 		return lang;
 	}
 
-	public void setLang(String lang) {
-		this.lang = lang;
+	/**
+	 * Set the language of the entry.
+	 * @param l the language to set
+	 */
+	public void setLang(final String l) {
+		this.lang = l;
 	}
 
+	/**
+	 * Return the ID of the entry.
+	 * @return the ID of the entry.
+	 */
+	public String getId() {
+		return id;
+	}
+
+	/**
+	 * Set the ID of the entry.
+	 * @param i the ID to set.
+	 */
+	public void setId(final String i) {
+		this.id = i;
+	}
+
+	/**
+	 * Return the root forms of the entry.
+	 * @return root forms of the entry
+	 */
 	public List<String> getRoots() {
 		return roots;
 	}
 
-	public void setRoots(List<String> roots) {
-		this.roots = roots;
+	/**
+	 * Set the root forms of the entry.
+	 * @param r the root forms to set
+	 */
+	public void setRoots(final List<String> r) {
+		this.roots = r;
 	}
 
+	/**
+	 * Return the inflected forms of the entry.
+	 * @return inflected forms of the entry
+	 */
 	public List<String> getForms() {
 		return forms;
 	}
 
-	public void setForms(List<String> forms) {
-		this.forms = forms;
+	/**
+	 * Set the inflected form of the entry.
+	 * @param f the inflected forms to set
+	 */
+	public void setForms(final List<String> f) {
+		this.forms = f;
 	}
 
+	/**
+	 * Return translations of the entry.
+	 * @return translations of the entry
+	 */
 	public List<String> getTrans() {
 		return trans;
 	}
 
-	public void setTrans(List<String> trans) {
-		this.trans = trans;
+	/**
+	 * Set translations of the entry.
+	 * @param t the translations to set
+	 */
+	public void setTrans(final List<String> t) {
+		this.trans = t;
 	}
 
+	/**
+	 * Return usage examples for the entry.
+	 * @return usage examples for the entry
+	 */
 	public List<String> getQuote() {
 		return quote;
 	}
 
-	public void setQuote(List<String> quote) {
-		this.quote = quote;
+	/**
+	 * Set usage examples of the entry.
+	 * @param q the usage examples to set
+	 */
+	public void setQuote(final List<String> q) {
+		this.quote = q;
 	}
 
+	/**
+	 * Return translations of usage examples.
+	 * @return translations of usage examples
+	 */
 	public List<String> getQuoteTrans() {
 		return quoteTrans;
 	}
 
-	public void setQuoteTrans(List<String> quoteTrans) {
-		this.quoteTrans = quoteTrans;
+	/**
+	 * Set translations of usage examples.
+	 * @param qt the translations of usage examples to set
+	 */
+	public void setQuoteTrans(final List<String> qt) {
+		this.quoteTrans = qt;
 	}
 
+	/**
+	 * Return the HTML content of the entry.
+	 * @return the HTML content of the entry
+	 */
 	public String getText() {
 		return text;
 	}
 
-	public void setText(String text) {
-		this.text = text;
+	/**
+	 * Set the HTML content of the entry.
+	 * @param t the HTML content to set
+	 */
+	public void setText(final String t) {
+		this.text = t;
 	}
 
 }
