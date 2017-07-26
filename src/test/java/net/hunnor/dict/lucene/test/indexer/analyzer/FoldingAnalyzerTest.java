b9b6b51f8e4ee95faefc73a45fa9d282bbc62d68
Ádám Z. Kövér <adamzkover@gmail.com>
2017-07-26 16:46:11 +0200

    Tests for FoldingAnalyzer

--- src/test/java/net/hunnor/dict/lucene/test/indexer/analyzer/FoldingAnalyzerTest.java
+++ src/test/java/net/hunnor/dict/lucene/test/indexer/analyzer/FoldingAnalyzerTest.java
@@ -1,6 +1,14 @@
 package net.hunnor.dict.lucene.test.indexer.analyzer;
 
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+
 import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
 import org.apache.lucene.util.Version;
 import org.junit.Test;
 
@@ -13,10 +21,20 @@ public final class FoldingAnalyzerTest {
 
 	/**
 	 * Test for the constructor with the version field.
+	 * @throws IOException 
 	 */
 	@Test
-	public void analyzerTest() {
+	public void analyzerTest() throws IOException {
 		Analyzer analyzer = new FoldingAnalyzer(Version.LUCENE_36);
+		Reader reader = new StringReader("åaøo; ÆaE");
+		TokenStream stream = analyzer.tokenStream("id", reader);
+		stream.incrementToken();
+		CharTermAttribute attribute =
+				stream.getAttribute(CharTermAttribute.class);
+		assertEquals("aaoo", attribute.toString());
+		stream.incrementToken();
+		attribute = stream.getAttribute(CharTermAttribute.class);
+		assertEquals("aeae", attribute.toString());
 		analyzer.close();
 	}
 
