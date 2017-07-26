dbafbb59a6e224fcbf5b650419a10e50db05c4ae
Ádám Z. Kövér <adamzkover@gmail.com>
2017-07-26 08:46:16 +0200

    Tests for PerFieldAnalyzer

---
+++ src/test/java/net/hunnor/dict/lucene/test/indexer/analyzer/PerFieldAnalyzerTest.java
@@ -0,0 +1,38 @@
+package net.hunnor.dict.lucene.test.indexer.analyzer;
+
+import static org.junit.Assert.assertTrue;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Modifier;
+
+import org.junit.Test;
+
+import net.hunnor.dict.lucene.indexer.analyzer.PerFieldAnalyzer;
+
+/**
+ * Tests for the custom Lucene analyzer.
+ */
+public class PerFieldAnalyzerTest {
+
+	/**
+	 * Test if the default constructor is private.
+	 * @throws SecurityException 
+	 * @throws NoSuchMethodException 
+	 * @throws InvocationTargetException 
+	 * @throws IllegalArgumentException 
+	 * @throws IllegalAccessException 
+	 * @throws InstantiationException 
+	 */
+	@Test
+	public void testPrivateConstructor() throws NoSuchMethodException,
+			SecurityException, InstantiationException, IllegalAccessException,
+			IllegalArgumentException, InvocationTargetException {
+		Constructor<PerFieldAnalyzer> constructor =
+				PerFieldAnalyzer.class.getDeclaredConstructor();
+		assertTrue(Modifier.isPrivate(constructor.getModifiers()));
+		constructor.setAccessible(true);
+		constructor.newInstance();
+	}
+
+}
