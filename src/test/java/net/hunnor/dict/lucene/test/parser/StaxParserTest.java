e5b75171371ae2a99badea15b2637c84f946b52d
Ádám Z. Kövér <adamzkover@gmail.com>
2017-08-02 18:56:34 +0200

    Tests for examples

--- src/test/java/net/hunnor/dict/lucene/test/parser/StaxParserTest.java
+++ src/test/java/net/hunnor/dict/lucene/test/parser/StaxParserTest.java
@@ -22,29 +22,14 @@ public final class StaxParserTest {
 	 * @throws XMLStreamException when thrown by the parser
 	 */
 	@Test
-	public void testTrans() throws XMLStreamException {
+	public void testLblLbl() throws XMLStreamException {
 		StaxParser staxParser = new StaxParser();
-		staxParser.openFile("src/test/resources/xml/sample-trans.xml");
+		staxParser.openFile("src/test/resources/xml/sample-lbl-lbl.xml");
 		Entry entry = staxParser.next();
 		assertEquals("1", entry.getId());
 		Set<String> roots = new HashSet<>();
 		assertEquals(roots, entry.getRoots());
-		assertEquals(" trans1", entry.getText());
-	}
-
-	/**
-	 * Test parsing of sample data.
-	 * @throws XMLStreamException when thrown by the parser
-	 */
-	@Test
-	public void testTransTrans() throws XMLStreamException {
-		StaxParser staxParser = new StaxParser();
-		staxParser.openFile("src/test/resources/xml/sample-trans-trans.xml");
-		Entry entry = staxParser.next();
-		assertEquals("1", entry.getId());
-		Set<String> roots = new HashSet<>();
-		assertEquals(roots, entry.getRoots());
-		assertEquals(" trans1, trans2", entry.getText());
+		assertEquals(" <i>lbl1</i> <i>lbl2</i>", entry.getText());
 	}
 
 	/**
@@ -67,6 +52,36 @@ public final class StaxParserTest {
 	 * @throws XMLStreamException when thrown by the parser
 	 */
 	@Test
+	public void testLblEg() throws XMLStreamException {
+		StaxParser staxParser = new StaxParser();
+		staxParser.openFile("src/test/resources/xml/sample-lbl-eg.xml");
+		Entry entry = staxParser.next();
+		assertEquals("1", entry.getId());
+		Set<String> roots = new HashSet<>();
+		assertEquals(roots, entry.getRoots());
+		assertEquals(" <i>lbl1</i>; <b>q1</b> trans1", entry.getText());
+	}
+
+	/**
+	 * Test parsing of sample data.
+	 * @throws XMLStreamException when thrown by the parser
+	 */
+	@Test
+	public void testTrans() throws XMLStreamException {
+		StaxParser staxParser = new StaxParser();
+		staxParser.openFile("src/test/resources/xml/sample-trans.xml");
+		Entry entry = staxParser.next();
+		assertEquals("1", entry.getId());
+		Set<String> roots = new HashSet<>();
+		assertEquals(roots, entry.getRoots());
+		assertEquals(" trans1", entry.getText());
+	}
+
+	/**
+	 * Test parsing of sample data.
+	 * @throws XMLStreamException when thrown by the parser
+	 */
+	@Test
 	public void testTransLbl() throws XMLStreamException {
 		StaxParser staxParser = new StaxParser();
 		staxParser.openFile("src/test/resources/xml/sample-trans-lbl.xml");
@@ -75,6 +90,66 @@ public final class StaxParserTest {
 		Set<String> roots = new HashSet<>();
 		assertEquals(roots, entry.getRoots());
 		assertEquals(" trans1, <i>lbl1</i>", entry.getText());
+	}
+
+	/**
+	 * Test parsing of sample data.
+	 * @throws XMLStreamException when thrown by the parser
+	 */
+	@Test
+	public void testTransTrans() throws XMLStreamException {
+		StaxParser staxParser = new StaxParser();
+		staxParser.openFile("src/test/resources/xml/sample-trans-trans.xml");
+		Entry entry = staxParser.next();
+		assertEquals("1", entry.getId());
+		Set<String> roots = new HashSet<>();
+		assertEquals(roots, entry.getRoots());
+		assertEquals(" trans1, trans2", entry.getText());
+	}
+
+	/**
+	 * Test parsing of sample data.
+	 * @throws XMLStreamException when thrown by the parser
+	 */
+	@Test
+	public void testTransEg() throws XMLStreamException {
+		StaxParser staxParser = new StaxParser();
+		staxParser.openFile("src/test/resources/xml/sample-trans-eg.xml");
+		Entry entry = staxParser.next();
+		assertEquals("1", entry.getId());
+		Set<String> roots = new HashSet<>();
+		assertEquals(roots, entry.getRoots());
+		assertEquals(" trans1; <b>q1</b> trans2", entry.getText());
+	}
+
+	/**
+	 * Test parsing of sample data.
+	 * @throws XMLStreamException when thrown by the parser
+	 */
+	@Test
+	public void testEgEg() throws XMLStreamException {
+		StaxParser staxParser = new StaxParser();
+		staxParser.openFile("src/test/resources/xml/sample-eg-eg.xml");
+		Entry entry = staxParser.next();
+		assertEquals("1", entry.getId());
+		Set<String> roots = new HashSet<>();
+		assertEquals(roots, entry.getRoots());
+		assertEquals(" <b>q1</b> trans1; <b>q2</b> trans2", entry.getText());
+	}
+
+	/**
+	 * Test parsing of sample data.
+	 * @throws XMLStreamException when thrown by the parser
+	 */
+	@Test
+	public void testQQ() throws XMLStreamException {
+		StaxParser staxParser = new StaxParser();
+		staxParser.openFile("src/test/resources/xml/sample-q-q.xml");
+		Entry entry = staxParser.next();
+		assertEquals("1", entry.getId());
+		Set<String> roots = new HashSet<>();
+		assertEquals(roots, entry.getRoots());
+		assertEquals(" <b>q1</b>, <b>q2</b> trans1", entry.getText());
 	}
 
 }
