8ab511f47c258cde3b823d00a57190329dd17c90
Ádám Z. Kövér <adamzkover@gmail.com>
2018-02-17 17:53:41 +0100

    Test coverage tool should ignore private constructors that do not have arguments

--- export-lucene-common/src/test/java/net/hunnor/dict/lucene/analyzer/PerFieldAnalyzerTest.java
+++ export-lucene-common/src/test/java/net/hunnor/dict/lucene/analyzer/PerFieldAnalyzerTest.java
@@ -1,7 +1,6 @@
 package net.hunnor.dict.lucene.analyzer;
 
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
 
 import net.hunnor.dict.lucene.analyzer.PerFieldAnalyzer;
 import net.hunnor.dict.lucene.constants.Lucene;
@@ -9,18 +8,7 @@ import net.hunnor.dict.lucene.constants.Lucene;
 import org.apache.lucene.analysis.Analyzer;
 import org.junit.Test;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Modifier;
-
 public class PerFieldAnalyzerTest {
-
-  @Test
-  public void testPrivateConstructor() throws Exception {
-    Constructor<PerFieldAnalyzer> constructor = PerFieldAnalyzer.class.getDeclaredConstructor();
-    assertTrue(Modifier.isPrivate(constructor.getModifiers()));
-    constructor.setAccessible(true);
-    constructor.newInstance();
-  }
 
   @Test
   public void testGetInstance() {
